39c01b863534ed722b16a790002e2aad276c26c3
Johan Roberto Rueda <jrrg4543@outlook.com>
2023-12-24 17:31:10 -0500

    Modify: Gtag and Heroku Procfile

--- src/main.js
+++ src/main.js
@@ -11,6 +11,8 @@ import App from './App.vue';
 // Vue.use(VueMeta);
 import "./assets/scss/custom.scss";
 
+// import { Client, Account, ID } from 'appwrite';
+
 import HomeComponent from "./components/Home.vue";
 import AboutComponent from "./components/Sections/About.vue";
 import ContactComponent from "./components/Sections/Contact.vue";
@@ -31,10 +33,17 @@ const router = createRouter({
     routes,
 });
 
+// const AppwriteClient = new Client()
+//     .setEndpoint(process.env.APPWRITE_HOST) // Your API Endpoint
+//     .setProject('[PROJECT_ID]');               // Your project ID
+// // appwrite
+// //     .setEndpoint()
+// //     .setProject(process.env.APPWRITE_PROJECT);
+
 createApp(App).use(i18n).use(router).use(VueGtag, {
     appName: 'JRuedaDev WebApp',
     pageTrackerScreenviewEnabled: true,
-    config: { id: "G-9VFF5S5Z1G" }
+    config: { id: "G-32E1WC96RZ" }
 }, router).mount('#app');
 
 import "bootstrap/dist/js/bootstrap.js";
