ed1fe1a84250e94124b0a6de51cd6494245e3b2c
Johan Roberto Rueda <jrrg4543@outlook.com>
2024-01-20 14:58:16 -0500

    Express Serve base app

---
+++ server-prod.js
@@ -0,0 +1,18 @@
+// Import the express and vite-express modules
+import express from "express"
+import ViteExpress from "vite-express"
+
+// Create an instance of the express application
+const app = express()
+
+// Define a route for the application
+app.get("/message", (_, res) => res.send("Hello from express!"(
+
+// Configure vite-express to serve the dist folder
+ViteExpress.serve(app, "dist")
+
+// Start the server on port 80
+ViteExpress.listen(app, 80, () => console.log("Server is listening on port 80..."))
+
+// Start the server on port 443
+ViteExpress.listen(app, 443, () => console.log("Server is listening on port 443..."))
