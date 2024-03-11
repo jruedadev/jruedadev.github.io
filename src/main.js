bfd45aea4dd1942d92b42c0bf696860f5719fe75
Johan Roberto Rueda <jrrg4543@outlook.com>
2024-03-10 21:01:00 -0500

    Added Sentry

--- src/main.js
+++ src/main.js
@@ -6,6 +6,7 @@ import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
 
+import * as Sentry from "@sentry/vue";
 /**
  * Custom Libraries
  */
@@ -13,7 +14,26 @@ import VueGtag from 'vue-gtag'
 import i18n from './i18n'
 
 const app = createApp(App)
-console.log(import.meta.env)
+
+Sentry.init({
+  dsn: import.meta.env.VITE_SENTRY_DSN,
+  integrations: [
+    Sentry.browserTracingIntegration(),
+    Sentry.replayIntegration({
+      maskAllText: false,
+      blockAllMedia: false,
+    }),
+  ],
+  // Performance Monitoring
+  tracesSampleRate: 1.0, //  Capture 100% of the transactions
+  // Set 'tracePropagationTargets' to control for which URLs distributed tracing should be enabled
+  tracePropagationTargets: ["localhost", /^https:\/\/yourserver\.io\/api/],
+  // Session Replay
+  replaysSessionSampleRate: 0.1, // This sets the sample rate at 10%. You may want to change it to 100% while in development and then sample at a lower rate in production.
+  replaysOnErrorSampleRate: 1.0, // If you're not already sampling the entire session, change the sample rate to 100% when sampling sessions where errors occur.
+});
+
+// console.log(import.meta.env)
 
 app.use(createPinia())
 app.use(router)
